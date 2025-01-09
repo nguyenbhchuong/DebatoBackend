bc80443d6d21370a126cda7b3f69fd37678a641c
tearecoil <ldthuc0605@gmail.com>
2025-01-09 21:40:13 +0700

    need JWT and connect from Frontend

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
-import express, {Application} from 'express';
-import authRotes from './routes/auth';
-import CookieParser from 'cookie-parser';
-import cors from 'cors';
-dotenv.config();
 
-
-const MongoDB_connection_string = 'mongodb://localhost';
-
-async function connecttoMongoDB(connection_string: string) {
-  await mongoose.connect(connection_string);
-  console.log('Connected');
-}
-try {
-  connecttoMongoDB(MongoDB_connection_string);
-} catch (e) {
-  console.log('Error: ', e)
-}
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
